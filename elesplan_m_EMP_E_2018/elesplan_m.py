b35b51e84c56f98b9180869985f6620114712e25
gplessm <guido.plessmann@rl-institut.de>
2018-06-07 00:09:39 +0200

    Introduce CLI

--- elesplan_m_EMP_E_2018/elesplan_m.py
+++ elesplan_m_EMP_E_2018/elesplan_m.py
@@ -1,5 +1,8 @@
 from elesplan_m_EMP_E_2018.model import ElesplanMOneYearModel
 from oemof import solph
+import argparse, sys
+import json
+import os
 
 
 def elesplan_m(data_path, year, scenario, debug=False):
@@ -22,12 +25,27 @@ def elesplan_m(data_path, year, scenario, debug=False):
     model_2020.solve()
 
 
+def elesplan_m_cmd():
+
+    parser = argparse.ArgumentParser(
+        description="Run elesplan-m for EMP 2018." + \
+                    "Available scenarios are listed in "
+                    "\n <link-to-table-in-repo-wiki>",
+        # epilog=example_text,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('data_path',
+                        type=str,
+                        help='Path to folder containing scenario files.')
+
+    args = parser.parse_args(sys.argv[1:])
+
+    # read config
+    config = json.load(open(os.path.join(args.data_path, 'config.cfg')))
+
+    elesplan_m(os.path.join(args.data_path, 'data'), config['years'][0],
+               config['scenario'])
+
+
 if __name__ == '__main__':
-
-    data_path = '/home/guido/rli_local/elesplan_m_EMP_E_2018_data/data/csv/' \
-                'EMP-E_2018-3Regions-2H'
-
-    year = 2020
-    scenario = 'EMP-E_2018-3Regions-2H'
-
-    elesplan_m(data_path, year, scenario)
+    pass
